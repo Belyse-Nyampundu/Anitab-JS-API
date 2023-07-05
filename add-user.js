9568728c284b011a29c0c9bfdcd2f90380d6e4cd
Belyse <belysenyampundu@gmail.com>
2023-07-05 14:04:54 +0300

    it contains first API in JavaScript

---
+++ add-user.js
@@ -0,0 +1,32 @@
+document.getElementById('form').addEventListener('submit', async function(event){
+event.preventDefault();//without it when I fill my form and submit I will still have my data
+
+
+let firstName = document.getElementById('firstName').value;
+let lastName = document.getElementById('lastName').value;
+let age = document.getElementById('age').value;
+
+let data ={
+    firstName:firstName,
+    lastName:lastName,
+    age:age
+
+}
+console.log({data});
+
+let result = await fetch('https://dummyjson.com/users/add', {
+  method: "POST",
+  headers:{
+    'content-Type' :'application/json'
+  },
+  body: JSON.stringify(data)
+
+})
+.then(response  =>response.json())
+.then(response => response)
+.catch(error => error.message)
+let success = document.getElementById('success');
+result&&result.id? success.innerHTML = 'user created successfully':
+success.innerHTML = 'User not created'
+console.log({result});
+})
